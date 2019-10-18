613d1ebf198f247c40248e8f456753a1fd537dc7
ngaivoronski <ngaivoronski@gmail.com>
2019-10-17 22:01:24 -0400

    styling

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { withFormik, Form, Field } from 'formik';
 import * as Yup from "yup";
 import axios from "axios";
 import './App.css';
+import styled from "styled-components";
 import ErrorWithStack from 'jest-util/build/ErrorWithStack';
 
 const OnboardingForm = ({values, touched, errors, status}) => {
@@ -12,10 +13,12 @@ const OnboardingForm = ({values, touched, errors, status}) => {
     status && setUsers(users => [...users, status])
   }, [status])
 
-
   return (
-    <div className="onboardingForm">
-      <Form>
+    <>
+    <div>
+      <h1>Onboarding Sig-up:</h1>
+      <Form className="onboardingForm">
+        <h2>Please enter your onboarding information:</h2>
         <Field type="text" name="name" placeholder="Name"/>
         {touched.name && errors.name && (
           <p>{errors.name}</p>
@@ -31,24 +34,34 @@ const OnboardingForm = ({values, touched, errors, status}) => {
           <p>{errors.password}</p>
         )}
 
+        <div className="TOS-field">
+        <label for="TOS"><p>I have read and agree with the Terms of Service</p></label>
         <Field type="checkbox" name="TOS" />
+        </div>
         {touched.TOS && errors.TOS && (
           <p>{errors.TOS}</p>
         )}
 
-        <button type="submit">Submit!</button>
+        <button type="submit">Submit</button>
       </Form>
-
-      {users.map(user => (
-        <div key={user.id}>
-          <p>Name: {user.name}</p>
-          <p>{user.email}</p>
-          <p>{user.password}</p>
-          <p>{user.TOS}</p>
-        </div>
-      ))}
-
     </div>
+    
+    <h1>Users List:</h1>
+
+    <div className="usersList">
+        {users.map(user => (
+          <div key={user.id} className="userCard">
+            <div className="userText">
+              <h2>User: {user.id}</h2>
+              <p>Name: {user.name}</p>
+              <p>Email: {user.email}</p>
+              <p>Password: {"*".repeat(user.password.length)}</p>
+              <p>{user.TOS}</p>
+            </div>
+          </div>
+        ))}
+    </div>
+    </>
 
   );
 }
